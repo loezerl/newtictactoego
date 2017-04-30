e8a71779cc30b71a925580e24376b3ce24fe28ea
Lucas Loezer <loezer.lucas@gmail.com>
2017-04-30 19:04:43 -0300

    Added more one loop to the second menu

--- client.go
+++ client.go
@@ -1,14 +1,15 @@
 package main
 
-import "io"
-import "os"
-import "fmt"
-import "net"
-import "encoding/json"
-import "net/textproto"
-import "bufio"
-import "time"
-//import "strconv"
+import(
+	 "io"
+	 "os"
+	 "fmt"
+	 "net"
+	 "encoding/json"
+	 "net/textproto"
+	 "bufio"
+	 "time"
+)
 
 type Message struct {
     Name string
@@ -36,18 +37,36 @@ func main () {
 	tp := textproto.NewReader(reader)
 	for{
 		if quit{ break }
-
+		quit2:= false
 		choice := PrintInitialInterface()
 		switch choice{
 			case 1: // start a new game
-				line, _ := tp.ReadLine()
-			 	var m Message
-				err = json.Unmarshal([]byte(line), &m)
-				if err != nil{
-					fmt.Println("Erro em descompactar o json")
+				for{
+					if quit2{break}
+					choice2 := PrintSecondInterface()
+					switch choice2{
+						case 1: //Make a play
+							line, _ := tp.ReadLine()
+						 	var m Message
+							err = json.Unmarshal([]byte(line), &m)
+							if err != nil{
+								fmt.Println("Erro em descompactar o json")
+							}
+						 	fmt.Println(m)
+						 	time.Sleep(time.Second * 2)
+						case 2: 
+							quit2 = true
+							break
+						case 3:
+							fmt.Println("Your choice isn't avaliable")
+							time.Sleep(time.Second * 2)
+							break
+						default:
+							fmt.Println("Your choice isn't avaliable")
+							time.Sleep(time.Second * 2)
+							break
+					}										
 				}
-			 	fmt.Println(m)
-			 	time.Sleep(time.Second * 2)
 			case 2: //exit for
 				quit = true
 				break
@@ -75,10 +94,28 @@ func mustCopy (dst io.Writer, src io.Reader) {
 
 func PrintInitialInterface() int{
 	fmt.Println("\033[H\033[2J")
-	fmt.Println("===== TIC TAC TOE =====")
+	fmt.Println("===== TIC TAC TOE =====\n")
 	fmt.Println("(1) - New Game")
 	fmt.Println("(2) - Exit")
-	fmt.Println("=======================")
+	fmt.Println("\n=======================")
+	var i int
+	fmt.Print("Your choice -> ")
+	_, err := fmt.Scanf("%d", &i)
+
+	if err != nil{
+		fmt.Println("Scanf error: ", err)
+		return 3
+	}
+	return i
+}
+
+func PrintSecondInterface() int{
+	fmt.Println("\033[H\033[2J")
+	fmt.Println("===== GAME =====\n")
+	fmt.Println("-- Actual Board --")
+	fmt.Println("(1) - Make a play")
+	fmt.Println("(2) - Back (will reset the game)")
+	fmt.Println("\n================")
 	var i int
 	fmt.Print("Your choice -> ")
 	_, err := fmt.Scanf("%d", &i)
